a2570fe59a6d1a5c6e450c29da254d572539ec6d
Shaha00 <ahmadoshahzod438@gmail.com>
2023-02-09 23:07:50 +0600

    Test

--- Test_Project/urls.py
+++ Test_Project/urls.py
@@ -20,7 +20,15 @@ from product import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/test/', views.test),
-    path('api/v1/products/', views.products_view),
+    path('api/v1/products/', views.ProductListCreateAPIView.as_view()),
     path('api/v1/products/<int:id>/', views.product_detail_view),
-    path('api/v1/profiles/', include('profiles.urls'))
+    path('api/v1/profiles/', include('profiles.urls')),
+    path('api/v1/categories/', views.CategoryListAPIView.as_view()),
+    path('api/v1/tags/', views.TagModelViewSet.as_view({
+        'get': 'list', 'post': 'create'
+    })),
+    path('api/v1/tags/<int:id>', views.TagModelViewSet.as_view({
+        'get': 'retrieve', 'put': 'update', 'delete': 'destroy'
+    })),
+    path('api/v1/categories/<int:id>/', views.CategoryDetailAPIView.as_view())
 ]
--- product/views.py
+++ product/views.py
@@ -1,11 +1,56 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .serializers import ProductSerializer, ProductDetailSerializer, ProductValidateSerializer
-from .models import Product
+from .serializers import ProductSerializer, ProductDetailSerializer, ProductValidateSerializer, CategorySerializer, TagSerializer
+from .models import Product, Category, Tag
 from rest_framework import status
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.viewsets import ModelViewSet
 
 
-@api_view(['GET', 'POST', 'PUT'])
+class CategoryListAPIView(ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    pagination_class = PageNumberPagination
+
+
+class CategoryDetailAPIView(RetrieveUpdateDestroyAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    lookup_field = 'id'
+
+
+class TagModelViewSet(ModelViewSet):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+    pagination_class = PageNumberPagination
+    lookup_field = 'id'
+
+
+class ProductListCreateAPIView(ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = ProductValidateSerializer(data=request.data)
+        if not serializer.is_valid():
+            return Response(data={"errors": serializer.errors},
+                            status=status.HTTP_406_NOT_ACCEPTABLE)
+        title = serializer.validated_data.get('title')
+        price = serializer.validated_data.get('price')
+        quantity = serializer.validated_data.get('quantity')
+        category_id = serializer.validated_data.get('category_id')
+        tags = serializer.validated_data.get('tags')
+
+        product = Product.objects.create(title=title, price=price, quantity=quantity, category_id=category_id)
+
+        product.tags.set(tags)
+        product.save()
+        return Response(data=ProductDetailSerializer(product).data,
+                        status=status.HTTP_201_CREATED)
+
+
+@api_view(['GET', 'POST'])
 def products_view(request):
     if request.method == 'GET':
         products = Product.objects.all()
